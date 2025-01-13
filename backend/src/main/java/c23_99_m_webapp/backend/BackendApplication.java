ea19ddaacd85337c270f3544065aac9e93f09e93
JuliaDaniR <julidrz@hotmail.com.ar>
2025-01-13 08:46:21 -0300

    Agregue security y login

--- backend/src/main/java/c23_99_m_webapp/backend/BackendApplication.java
+++ backend/src/main/java/c23_99_m_webapp/backend/BackendApplication.java
@@ -7,32 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication implements CommandLineRunner {
+public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		mostrarRol();
-	}
-
-	// Método para probar la asignación de roles
-	public void mostrarRol() {
-		User user = new User();
-
-		// Probar diferentes roles
-		user.setRole(Role.USER);
-		System.out.println(user.getRole() + ": " + user.getAuthorities());
-
-		user.setRole(Role.ADMIN);
-		System.out.println(user.getRole() + ": " + user.getAuthorities());
-
-		user.setRole(Role.TEACHER);
-		System.out.println(user.getRole() + ": " + user.getAuthorities());
-
-		user.setRole(Role.SUPERADMIN);
-		System.out.println(user.getRole() + ": " + user.getAuthorities());
-	}
 }
