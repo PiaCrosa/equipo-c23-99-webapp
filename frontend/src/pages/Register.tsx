32aeef6c91558bbc0178f12fcf4b6c237d2f8202
Domenico Pagano <dpaganoh@gmail.com>
2025-01-17 21:24:10 -0500

    formulario

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,11 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { validateForm } from '../utils/validations';
+import logo from '../assets/img/logo-box.svg';
 
-const registerContainer = "flex flex-col items-center justify-center bg-zinc-50 px-4";
+const registerContainer = "flex items-center justify-center gap-20 bg-zinc-50 px-4 pt-16";
 const formElement = "bg-white p-6 rounded-lg shadow-md w-full max-w-md my-10";
-const inputField = "mb-4 p-2 border border-gray-300 rounded w-full";
+const inputField = "mb-4 p-1.5 border border-gray-300 rounded w-full";
 const submitButton = "mt-6 bg-sky-500 text-white p-2 rounded w-full hover:bg-sky-400";
+const leftText = "text-sky-500 font-sans text-5xl pb-4";
+const leftTextSimple = "text-sky-500 font-sans text-xl pb-4 text-justify leading-relaxed";
 
 const Register: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -52,6 +55,18 @@ const Register: React.FC = () => {
       <Header />
       
       <div className={registerContainer}>
+
+        <div className='flex flex-col gap-2'>
+          <div>
+            <img src={logo} alt="classkit box logo" className='w-[600px] opacity-[0.05] fixed top-[20vh] left-10'/>
+            <p className={leftText}>Bienvenido a Class<span className='text-orange-400 italic'>Kit</span></p>
+          </div>
+          <div className='max-w-[500px]'>
+            <p className={leftTextSimple}>Optimiza la gestión de los recursos de tu institución en un solo lugar. Regístrate como administrador para comenzar a crear y gestionar tu inventario de manera eficiente. ¡Facilita el acceso a proyectores, mapas y más para tus profesores con solo unos clics!</p>
+            <p className={leftTextSimple}>Completa el registro y transforma la forma en que administras los recursos educativos.</p>
+          </div>
+        </div>
+
         <form className={formElement} onSubmit={handleSubmit}>
           <h2 className="text-2xl font-medium mb-6 text-sky-500">Registrar Administrador</h2>
           <input type="text" name="fullName" placeholder="Nombre completo" className={inputField} onChange={handleChange} />
