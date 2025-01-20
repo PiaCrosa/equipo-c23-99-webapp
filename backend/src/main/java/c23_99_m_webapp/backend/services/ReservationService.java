60b71f64867686577a7b6b085cd7b5b0ca4c0cb9
LNPoly <paula.dominguez2205@gmail.com>
2025-01-20 12:44:39 -0300

    changes in variables and reservation methods

--- backend/src/main/java/c23_99_m_webapp/backend/services/ReservationService.java
+++ backend/src/main/java/c23_99_m_webapp/backend/services/ReservationService.java
@@ -6,6 +6,7 @@ import c23_99_m_webapp.backend.models.Resource;
 import c23_99_m_webapp.backend.models.User;;
 import c23_99_m_webapp.backend.models.dtos.DataAnswerReservation;
 import c23_99_m_webapp.backend.models.dtos.ReservationDto;
+import c23_99_m_webapp.backend.models.dtos.ResourceDTO;
 import c23_99_m_webapp.backend.models.enums.ReservationShiftStatus;
 import c23_99_m_webapp.backend.models.enums.ReservationStatus;
 import c23_99_m_webapp.backend.repositories.ReservationRepository;
@@ -35,40 +36,42 @@ public class ReservationService {
     @Autowired
     ResourceRepository resourceRepository;
 
-//   METODO CREATE CON RELACION A USER
-public DataAnswerReservation createdReservation(ReservationDto reservationDto) throws MyException {
+//  METODO CREATE CON RELACION A USER
+    public DataAnswerReservation createdReservation(ReservationDto reservationDto) throws MyException {
 
-    UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    User user = userRepository.findByEmail(userDetails.getUsername());
-    System.out.println(user.getFullName());
-    if (user == null || !user.getActive()) {
-        throw new MyException("User  not found.");
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByEmail(userDetails.getUsername());
+        System.out.println(user.getFullName());
+        if (user == null || !user.getActive()) {
+            throw new MyException("User  not found.");
+        }
+        // validar estado del recurso
+        Optional<Resource> resourceOptional = resourceRepository.findById(reservationDto.resourceid());
+        Resource resource = resourceOptional.get();
+
+        System.out.println(resource);
+        Reservation reservation = new Reservation();
+        reservation.setCountElement(reservationDto.countElement());
+        reservation.setStartDate(reservationDto.startDate());
+        reservation.setReservationShiftStatus(reservationDto.reservationShiftStatus());
+        reservation.setSelectedTimeSlot(reservationDto.selectedTimeSlot());
+        reservation.setReservationStatus(ReservationStatus.CONFIRMED);
+        reservation.setDeleted(false);
+        reservation.setUser(user);
+        reservation.setResource(resource);
+
+        reservation = reservationRepository.save(reservation);
+
+        DataAnswerReservation data = new DataAnswerReservation(
+                reservation.getStartDate(),
+                reservation.getReservationShiftStatus(),
+                reservation.getSelectedTimeSlot(),
+                reservation.getResource().getId(),
+                reservation.getReservationStatus()
+        );
+
+        return data;
     }
-
-    Optional<Resource> resourceOptional = resourceRepository.findById(reservationDto.resourceid());
-    Resource resource = resourceOptional.get();
-
-    System.out.println(resource);
-    Reservation reservation = new Reservation();
-    reservation.setCountElement(reservationDto.countElement());
-    reservation.setStartDate(reservationDto.startDate());
-    reservation.setStartHour(reservationDto.starHour());
-    reservation.setReservationStatus(ReservationStatus.CONFIRMED);
-    reservation.setDeleted(false);
-    reservation.setUser(user);
-    reservation.setResource(resource);
-
-    reservation = reservationRepository.save(reservation);
-
-    DataAnswerReservation data = new DataAnswerReservation(
-            reservation.getStartDate(),
-            reservation.getStartHour(),
-            reservation.getResource().getId(),
-            reservation.getReservationStatus()
-    );
-
-    return data;
-}
     public List<ReservationDto> getReservations () {
         return reservationRepository.findAll().stream()
                 .map(this::convertToDto)
@@ -85,16 +88,26 @@ public DataAnswerReservation createdReservation(ReservationDto reservationDto) t
         return new ReservationDto(
                 reservation.getCountElement(),
                 reservation.getStartDate(),
-                reservation.getStartHour(),
+                reservation.getReservationShiftStatus(),
+                reservation.getSelectedTimeSlot(),
                 reservation.getResource().getId()
         );
     }
+
+//    METODO DISPONIBILIDAD DE RECURSO
+//    private void checkAvaiableResource(Resource resource){
+//        if(!reservationRepository.existsByResource(resource)){
+//            throw new RuntimeException("El material solicitado no está disponible.");
+//        }
+//    }
+
     public Optional<ReservationDto> updateById (Long id, ReservationDto updatedReservationDto){
         return reservationRepository.findById(id).map(reservation -> {
 
             reservation.setCountElement(updatedReservationDto.countElement());
             reservation.setStartDate(updatedReservationDto.startDate());
-            reservation.setStartHour(updatedReservationDto.starHour());
+            reservation.setReservationShiftStatus(updatedReservationDto.reservationShiftStatus());
+            reservation.setSelectedTimeSlot(updatedReservationDto.selectedTimeSlot());
             reservation.setReservationStatus(ReservationStatus.CONFIRMED);
 
             Reservation updatedReservation = reservationRepository.save(reservation);
