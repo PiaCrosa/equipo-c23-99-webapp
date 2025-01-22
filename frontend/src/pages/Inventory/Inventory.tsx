320c6c0131f7ef4f0e020d2135b6f3fba5db1fa0
gerammon <alvarezcopa591@gmail.com>
2025-01-22 17:34:04 -0400

    html structure

--- frontend/src/pages/Inventory/Inventory.tsx
+++ frontend/src/pages/Inventory/Inventory.tsx
@@ -1,8 +1,49 @@
 const Inventory = () => {
   return (
-    <div>
-      Inventory
-    </div>
+    <>
+      <div>
+        Buscar recurso
+      </div>
+      <div>
+        <label>
+          Nombre
+        </label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>
+          Categoría
+        </label>
+        <select>
+          <option value="">Opcion 1</option>
+        </select>
+      </div>
+      <div>
+        <button>
+          Buscar
+        </button>
+      </div>
+      <div>
+        <div>
+          <div>
+            Proyector
+          </div>
+          <div>
+            Descripcion
+          </div>
+          <div>
+            <div>
+              <button>
+                Editar
+              </button>
+              <button>
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
