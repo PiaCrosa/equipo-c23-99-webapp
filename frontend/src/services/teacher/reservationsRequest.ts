56e13a03105da53d0e82bce37dc1bea7e11c9a25
julio Rivas <juliorivasdev@gmail.com>
2025-01-30 19:45:31 -0300

    conflicto arreglado

--- frontend/src/services/teacher/reservationsRequest.ts
+++ frontend/src/services/teacher/reservationsRequest.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { PORT_SERVER } from '..';
 import { ReservationResponse } from '../../models/teacher/ReservationGetUser';
-import { ReservationResponse } from '../../models/teacher/ReservationGetUser';
 
 export const getReservationsByUser = async (
 	dni: string,
