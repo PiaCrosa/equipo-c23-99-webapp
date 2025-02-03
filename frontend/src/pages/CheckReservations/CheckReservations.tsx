d6e364a38bbe33ced5216e57850ca30547443d18
julio Rivas <juliorivasdev@gmail.com>
2025-02-03 20:26:37 -0300

    fixed bug in delete reserves

--- frontend/src/pages/CheckReservations/CheckReservations.tsx
+++ frontend/src/pages/CheckReservations/CheckReservations.tsx
@@ -22,22 +22,16 @@ const CheckReservations: React.FC = () => {
 		const fetchReservations = async () => {
 			const booking = await getReservationTeacher();
 
-			if (
-				booking &&
-				JSON.stringify(booking) !== JSON.stringify(prevReservationsRef.current)
-			) {
+			if (booking) {
 				const mergedReservations = mergeConsecutiveReservations(booking);
 				prevReservationsRef.current = booking;
 				setDataReservations(mergedReservations);
 			}
 		};
 		fetchReservations();
-	}, [
-		getReservationTeacher,
-		mergeConsecutiveReservations,
-		dataReservations,
-		indiReservations,
-	]);
+	}, [getReservationTeacher, mergeConsecutiveReservations]);
+
+	console.log(indiReservations);
 
 	return (
 		<div className='bg-gray-100 shadow-md rounded border border-gray-200 px-8 pt-8 pb-8 mb-4 m-8'>
@@ -45,15 +39,22 @@ const CheckReservations: React.FC = () => {
 				Mis Reservas
 			</h2>
 			<Reservations
+				modalOpen={isModalOpen}
 				dataReservations={dataReservations}
 				changeModal={setIsModalOpen}
 				changeIndiReservations={setIndiReservations}
+				setDataReserve={setDataReservations}
 			/>
 			<ModalCheckReservations
 				isOpen={isModalOpen}
 				onClose={() => setIsModalOpen(false)}
 				title={`Reservas del ${indiReservations ? revertDate(indiReservations[0].date) : ''}`}>
-				<Reservations dataReservations={indiReservations} />
+				<Reservations
+					modalOpen={isModalOpen}
+					dataReservations={indiReservations}
+					setDataReserve={setDataReservations}
+					changeIndiReservations={setIndiReservations}
+				/>
 			</ModalCheckReservations>
 		</div>
 	);
