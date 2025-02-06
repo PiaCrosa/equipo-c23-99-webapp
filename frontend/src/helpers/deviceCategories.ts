e429f2c5d67771ee71f7954456c6ffa46e248c0c
gerammon <alvarezcopa591@gmail.com>
2025-02-05 20:28:02 -0400

    device categories

--- frontend/src/helpers/deviceCategories.ts
+++ frontend/src/helpers/deviceCategories.ts
@@ -1,12 +1,21 @@
+import { CategoryType } from './CategoryType';
 import { MultipleFormControlOption } from './MultipleFormControlOption';
 
-export const deviceCategoriesOptions: MultipleFormControlOption[] = [
+export const deviceCategoriesOptions: MultipleFormControlOption<CategoryType>[] = [
   {
     commonName: 'Audiovisual',
     value: 'AUDIOVISUALS',
   },
   {
-    commonName: 'IT',
-    value: 'IT',
+    commonName: 'Clase',
+    value: 'CLASSROOM',
+  },
+  {
+    commonName: 'Material de Laboratorio',
+    value: 'LABORATORY_MATERIALS',
+  },
+  {
+    commonName: 'Modelos',
+    value: 'MODELS',
   },
 ] as const;
