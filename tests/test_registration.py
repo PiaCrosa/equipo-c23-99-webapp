bb06340928cdd263a1013c65e454fca20de15aa7
PiaCrosa <piacrosa27@gmail.com>
2025-06-04 21:03:23 -0300

    Agregué tests

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -17,5 +17,53 @@ def test_required_fields(registration_page, missing_field):
     registration_page.fill_registration_form(**test_data)
     registration_page.submit_form()
 
-    assert not registration_page.is_field_filled(getattr(registration_page, f"{missing_field}_input")), \
+    assert not registration_page.is_field_filled(getattr(registration_page, f"{missing_field}_field")), \
         f"El campo '{missing_field}' debería estar en estado inválido y mostrando tooltip"
+        
+
+def test_dni_seven_digits_error_message(registration_page):
+    test_data=generate_test_data()
+    test_data["DNI"]="1234567"
+    
+    registration_page.fill_registration_form(**test_data)
+    registration_page.submit_form()
+    
+    error_message=registration_page.get_error_message()
+    assert error_message=="El DNI es obligatorio y debe tener 8 caracteres numéricos", \
+        f"Se esperaba el mensaje de error 'El DNI es obligatorio y debe tener 8 caracteres numéricos', pero se recibió '{error_message}'"
+        
+        
+def test_dni_nine_digits_error_message(registration_page):
+    test_data=generate_test_data()
+    test_data["DNI"]="123456789"
+    
+    registration_page.fill_registration_form(**test_data)
+    registration_page.submit_form()
+    
+    error_message=registration_page.get_error_message()
+    assert error_message=="El DNI es obligatorio y debe tener 8 caracteres numéricos", \
+        f"Se esperaba el mensaje de error 'El DNI es obligatorio y debe tener 8 caracteres numéricos', pero se recibió '{error_message}'"
+        
+        
+def test_letters_phone_error_message(registration_page):
+    test_data=generate_test_data()
+    test_data["tel"]="letters"
+    
+    registration_page.fill_registration_form(**test_data)
+    registration_page.submit_form()
+    
+    error_message=registration_page.get_error_message()
+    assert error_message=="El teléfono es obligatorio y debe contener solo números", \
+        f"Se esperaba el mensaje de error 'El teléfono es obligatorio y debe contener solo números', pero se recibió '{error_message}'"
+        
+       
+def test_confirm_password_does_not_match_error_message(registration_page):
+    test_data=generate_test_data()
+    test_data["cpassword"]="Password2@"
+    
+    registration_page.fill_registration_form(**test_data)
+    registration_page.submit_form()
+    
+    error_message=registration_page.get_error_message()
+    assert error_message=="Las contraseñas no coinciden", \
+        f"Se esperaba el mensaje de error 'Las contraseñas no coinciden', pero se recibió '{error_message}'"   
